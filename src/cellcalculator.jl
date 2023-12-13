0b89021a74e9f1d54c486574ae8cabaa676b06c9
Ewout van der Veer <evanderveer@users.noreply.github.com>
2023-12-13 23:00:50 +0100

    Add docstrings

--- src/cellcalculator.jl
+++ src/cellcalculator.jl
@@ -1,5 +1,17 @@
 include("index_generator.jl")
 
+"""
+    struct CellParameters
+    Represents the parameters of a crystallographic cell.
+
+    Fields:
+    - `a::Real`: Length of the first cell vector.
+    - `b::Real`: Length of the second cell vector.
+    - `c::Real`: Length of the third cell vector.
+    - `α::Real`: Angle between the second and third cell vectors (in degrees).
+    - `β::Real`: Angle between the first and third cell vectors (in degrees).
+    - `γ::Real`: Angle between the first and second cell vectors (in degrees).
+"""
 struct CellParameters
     a::Real
     b::Real
@@ -9,6 +21,12 @@ struct CellParameters
     γ::Real
 end
 
+"""
+    orthonormal_basis_matrix(cell_parameters::CellParameters)
+    Compute the change of basis matrix to an orthonormal basis for the given cell parameters.
+
+    Returns a 3x3 matrix representing the orthonormal basis.
+"""
 function orthonormal_basis_matrix(
     cell_parameters::CellParameters
 )
@@ -23,6 +41,12 @@ function orthonormal_basis_matrix(
      0 0         cz]
 end
 
+"""
+    change_to_orthonormal_basis(vector::Vector{<:Real}, cell_parameters::CellParameters)
+    Transform a vector from the lattice vector basis to the orthonormal basis.
+
+    Returns the vector in the orthonormal basis.
+"""
 function change_to_orthonormal_basis(
     vector::Vector{<:Real}, 
     cell_parameters::CellParameters
@@ -31,6 +55,12 @@ function change_to_orthonormal_basis(
     CoBmatrix * vector
 end
 
+"""
+    change_from_orthonormal_basis(vector::Vector{<:Real}, cell_parameters::CellParameters)
+    Transform a vector from the orthonormal basis back to the lattice vector basis.
+
+    Returns the vector in the lattice vector basis.
+"""
 function change_from_orthonormal_basis(
     vector::Vector{<:Real}, 
     cell_parameters::CellParameters
@@ -41,6 +71,17 @@ end
 
 (yield_index, reset_index) = index_generator()
 
+"""
+    find_lower_index_zone(
+        zone_axis::Vector{<:Real},
+        cell_parameters::CellParameters,
+        tolerance::Real,
+        max_iterations::Int
+    )
+    Find a lower index zone axis given the initial zone axis and cell parameters.
+
+    Returns the lower index zone axis within the specified tolerance and maximum iterations.
+"""
 function find_lower_index_zone(
     zone_axis::Vector{<:Real}, 
     cell_parameters::CellParameters, 
@@ -71,6 +112,17 @@ function find_lower_index_zone(
     round.(Int, zone_axis)
 end
 
+"""
+    find_orthogonal_axis(
+        zone_axis::Vector{<:Real},
+        cell_parameters::CellParameters,
+        tolerance::Real,
+        max_iterations::Int
+    )
+    Find an orthogonal axis to the given zone axis within the specified tolerance and maximum iterations.
+
+    Returns the orthogonal axis.
+"""
 function find_orthogonal_axis(
     zone_axis::Vector{<:Real}, 
     cell_parameters::CellParameters, 
@@ -93,6 +145,16 @@ function find_orthogonal_axis(
 
 end
 
+"""
+    find_orthogonal_axis(
+        vector_1::Vector{<:Real},
+        vector_2::Vector{<:Real},
+        cell_parameters::CellParameters
+    )
+    Find an orthogonal axis given two vectors and cell parameters.
+
+    Returns the orthogonal axis.
+"""
 function find_orthogonal_axis(
     vector_1::Vector{<:Real}, 
     vector_2::Vector{<:Real}, 
@@ -104,6 +166,17 @@ function find_orthogonal_axis(
     change_from_orthonormal_basis(vector_1_orthogonal × vector_2_orthogonal, cell_parameters)
 end
 
+"""
+    find_orthogonal_cell(
+        zone_axis::Vector{<:Int},
+        cell_parameters::CellParameters;
+        tolerance::Real = 1,
+        max_iterations::Int = 10_000_000
+    )
+    Tries to find an orthogonal unit cell given a zone axis and cell parameters.
+
+    Returns the change of basis matrix from the given unit cell to the new orthogonal cell.
+"""
 function find_orthogonal_cell(
     zone_axis::Vector{<:Int},
     cell_parameters::CellParameters; 
@@ -135,6 +208,12 @@ function find_orthogonal_cell(
     [orthogonal_vector third_vector zone_axis]
 end
 
+"""
+    print_results(a, b, c, cell_parameters)
+    Print information about the new orthogonal cell.
+
+    Displays the lengths and angles of the orthogonal cell vectors.
+"""
 function print_results(a, b, c, cell_parameters)
     println("\nNew orthogonal cell found. a = $(Tuple(a)), b = $(Tuple(b)), c (ZA) = $(Tuple(c))")
 
@@ -142,6 +221,12 @@ function print_results(a, b, c, cell_parameters)
     println("Orthogonal cell angle deviation from 90°: α = $(angles[1])°, β = $(angles[2])°, γ = $(angles[3])°")
 end
 
+"""
+    find_vector_angles(vectors::Vector{<:Vector{<:Real}}, cell_parameters::CellParameters)
+    Calculate the angles between vectors in the orthonormal basis.
+
+    Returns a vector containing the angles α, β, and γ (in degrees).
+"""
 function find_vector_angles(
     vectors::Vector{<:Vector{<:Real}}, 
     cell_parameters::CellParameters
@@ -155,8 +240,35 @@ function find_vector_angles(
     [α, β, γ]
 end
 
-angle(a, b) = acosd(clamp(a ⋅ b / (norm(a) * norm(b)), -1, 1))
+"""
+    angle(a::Vector{<:Real}, b::Vector{<:Real})
+    Calculate the angle in degrees between two vectors a and b.
+"""
+function angle(
+    a::Vector{<:Real}, 
+    b::Vector{<:Real}
+    )
+    if length(a) != length(b)
+        throw(ArgumentError("Vectors must have the same length"))
+    end
 
+    dot_product = a ⋅ b
+    magnitude_product = norm(a) * norm(b)
+    
+    if magnitude_product ≈ 0.0
+        throw(ArgumentError("Cannot calculate angle with zero-length vector"))
+    end
+
+    dot_clamped = clamp(dot_product / magnitude_product, -1, 1)
+    return acosd(dot_clamped)
+end
+
+"""
+    load_cell(filename::String)
+    Load cell parameters and atom positions in the unit cell from a .cel file.
+
+    Returns a tuple containing the CellParameters and the atom positions.
+"""
 function load_cell(
     filename::String
 )
